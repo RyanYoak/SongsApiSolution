a39ae5d361d95c78f6afd4dce2c20c5304ecb80f
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-24 15:45:24 -0400

    Got some tests for ya

---
+++ SongsApiIntegrationTests/BasicWebApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SongsApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongsApiIntegrationTests
+{
+    public class BasicWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+
+    }
+}
